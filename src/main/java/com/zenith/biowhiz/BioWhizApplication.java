a56c123b6cb502001d942b23f0f9419b578a7b46
Huss4in007 <smdhussainmolla@gmail.com>
2023-05-12 18:02:41 +0600

    An upwork profile builder tool

---
+++ src/main/java/com/zenith/biowhiz/BioWhizApplication.java
@@ -0,0 +1,13 @@
+package com.zenith.biowhiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioWhizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioWhizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zenith/biowhiz/BioWhizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenith.biowhiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioWhizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
